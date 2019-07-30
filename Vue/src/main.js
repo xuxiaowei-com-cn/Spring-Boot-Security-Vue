5abfa59f34f53b8b055abda4b4023d2ee4cd7770
徐晓伟 <xuxiaowei@xuxiaowei.com.cn>
2019-07-30 12:40:53 +0800

    vue-router Configuration

--- Vue/src/main.js
+++ Vue/src/main.js
@@ -1,8 +1,10 @@
 import Vue from 'vue'
 import App from './App.vue'
+import router from './router' // 导入路由配置文件
 
-Vue.config.productionTip = false
+Vue.config.productionTip = false;
 
 new Vue({
-  render: h => h(App),
-}).$mount('#app')
+    router, // 启用路由配置文件
+    render: h => h(App),
+}).$mount('#app');
---
+++ Vue/src/router.js
@@ -0,0 +1,43 @@
+/**
+ * 路由配置文件
+ */
+import Vue from 'vue' // Vue，用于启用路由
+import Router from 'vue-router' // 导入路由依赖
+import Home from './views/Home.vue' // 页面
+import Login from './views/Login.vue'
+
+Vue.use(Router); // Vue 启用路由
+
+export default new Router({ // 创建默认路由
+    mode: 'history', // H5 history 路由
+    routes: [ // 路由配置
+        {
+            path: '/', // 路径
+            name: 'home', // title
+            component: Home, // Vue 页面
+            meta: {
+                title: 'Home'
+            }
+        }, {
+            path: '/login',
+            name: 'login',
+            component: Login,
+            meta: {
+                title: 'Login'
+            }
+        }, {
+            path: '/about',
+            name: 'about',
+            // route level code-splitting
+            // this generates a separate chunk (about.[hash].js) for this route
+            // which is lazy-loaded when the route is visited.
+            // 路由级代码分裂
+            // 为此路由生成一个单独的块 (about.[hash].js)
+            // 当访问路线时，它是延迟加载的。
+            component: () => import(/* webpack 块名称: "about" */ './views/About.vue'),
+            meta: {
+                title: 'About'
+            }
+        },
+    ]
+})
