82f63384b680e374f05f73f23fd0dacaa09beada
徐晓伟 <xuxiaowei@xuxiaowei.com.cn>
2019-07-30 15:20:02 +0800

    登录页面

--- Vue/src/views/Login.vue
+++ Vue/src/views/Login.vue
@@ -1,13 +1,79 @@
 <template>
-    <h2>Login</h2>
+    <el-container>
+        <el-header>Login</el-header>
+
+        <el-main>
+            <el-form ref="form" :model="form" class="form-login">
+
+                <el-form-item prop="username">
+                    <el-input placeholder="请输入用户名" prefix-icon="el-icon-user" v-model="form.username" ref="username"
+                              maxlength="11"></el-input>
+                </el-form-item>
+
+                <el-form-item prop="password">
+                    <el-input placeholder="请输入密码" prefix-icon="el-icon-lock" v-model="form.password" show-password
+                              maxlength="16"></el-input>
+                </el-form-item>
+
+                <el-form-item>
+                    <el-checkbox class="rememberMe" v-model="form.rememberMe">记住我</el-checkbox>
+                </el-form-item>
+
+                <el-form-item>
+                    <el-button class="login" type="primary" @click="login">登录</el-button>
+                </el-form-item>
+
+            </el-form>
+        </el-main>
+
+        <el-footer></el-footer>
+    </el-container>
 </template>
 
 <script>
     export default {
-        name: "Login"
+        name: "Login",
+        data() {
+            return {
+                form: {
+                    username: '',
+                    password: '',
+                    rememberMe: false,
+                },
+            }
+        },
+        methods: {
+
+            /**
+             * 登录
+             */
+            login: function () {
+                // let that = this;
+                let form = this.form;
+                // eslint-disable-next-line no-console
+                console.log(form)
+
+            },
+
+        },
     }
 </script>
 
 <style scoped>
+
+    .form-login {
+        max-width: 300px;
+        margin: auto auto;
+    }
+
+    /* 记住我 */
+    .rememberMe {
+        float: left;
+    }
+
+    /* 登录 */
+    .login {
+        width: 100%;
+    }
 
 </style>
