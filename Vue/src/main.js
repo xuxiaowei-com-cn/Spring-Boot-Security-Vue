4bc50b84c36f385895c07768272b56a2c05d3a2a
徐晓伟 <xuxiaowei@xuxiaowei.com.cn>
2019-07-30 12:42:30 +0800

    title Configuration

--- Vue/src/main.js
+++ Vue/src/main.js
@@ -4,6 +4,14 @@ import router from './router' // 导入路由配置文件
 
 Vue.config.productionTip = false;
 
+router.beforeEach((to, from, next) => {
+    /* 根据路由设置 title，路由发生改变修改页面的 title */
+    if (to.meta.title) {
+        document.title = to.meta.title
+    }
+    next();
+});
+
 new Vue({
     router, // 启用路由配置文件
     render: h => h(App),
