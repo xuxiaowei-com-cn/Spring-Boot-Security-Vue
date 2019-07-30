dc16af303df426a9fafdf27d6d43719e8e485ed0
徐晓伟 <xuxiaowei@xuxiaowei.com.cn>
2019-07-30 16:57:29 +0800

    自定义请求

--- Spring-Boot-Security/src/main/java/cn/com/xuxiaowei/filter/CsrfBeforeFilter.java
+++ Spring-Boot-Security/src/main/java/cn/com/xuxiaowei/filter/CsrfBeforeFilter.java
@@ -23,8 +23,12 @@ import org.springframework.web.filter.OncePerRequestFilter;
 import javax.servlet.FilterChain;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletRequestWrapper;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.util.Enumeration;
+import java.util.HashMap;
+import java.util.Map;
 
 /**
  * CSRF 策略 运行前 Filter
@@ -65,6 +69,62 @@ public class CsrfBeforeFilter extends OncePerRequestFilter {
 
 
         filterChain.doFilter(request, response);
+    }
+
+    /**
+     * 自定义请求
+     * <p>
+     * 为了简化代码，此内部类仅重写了后面要使用的方法，有兴趣的同学可以尝试重写其他方法
+     * <p>
+     * 使用 static 节省资源
+     *
+     * @author xuxiaowei
+     * @since 0.0.1
+     */
+    private static class CustomizeRequest extends HttpServletRequestWrapper {
+
+        private Map<String, String> headerMap;
+
+        /**
+         * 构造一个包装给定请求的请求对象。
+         *
+         * @param request HttpServletRequest
+         * @throws IllegalArgumentException 如果 request 为 null时，抛出此异常
+         */
+        private CustomizeRequest(HttpServletRequest request) {
+            super(request);
+
+            headerMap = new HashMap<>();
+
+            // 将原始 Headers 放入新 Headers（Map）中
+            Enumeration enumeration = request.getHeaderNames();
+            while (enumeration.hasMoreElements()) {
+                String name = (String) enumeration.nextElement();
+                headerMap.put(name, request.getHeader(name));
+            }
+        }
+
+        /**
+         * 由于{@link CsrfFilter#doFilterInternal(HttpServletRequest, HttpServletResponse, FilterChain)}中使用了{@link HttpServletRequest#getHeader(String)}，
+         * 所以此方法必须重写
+         * <p>
+         * 注：上述报错红，原因为该方法不是公共的，并非错误
+         */
+        @Override
+        public String getHeader(String name) {
+            return headerMap.get(name);
+        }
+
+        /**
+         * 此方法用于添加 Header
+         *
+         * @param name  需要添加的 Header name
+         * @param value 需要添加的 Header value
+         */
+        private void addHeader(String name, String value) {
+            headerMap.put(name, value);
+        }
+
     }
 
 }
