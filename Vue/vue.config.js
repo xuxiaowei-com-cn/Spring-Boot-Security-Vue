89629636b07b78e496e01bf44b306fa3080c9aa6
徐晓伟 <xuxiaowei@xuxiaowei.com.cn>
2020-02-23 09:24:46 +0800

    报错注释

--- Vue/vue.config.js
+++ Vue/vue.config.js
@@ -2,7 +2,7 @@ module.exports = {
     publicPath: '/', // 公共路径
     devServer: {
         public: "demo.xuxiaowei.com.cn", // 远程域名
-        host: "demo.xuxiaowei.com.cn", // 本地域名
+        // host: "demo.xuxiaowei.com.cn", // 本地域名
         port: 8001, // 端口
         // https: true, // https，默认关闭，暂未启动
     }
